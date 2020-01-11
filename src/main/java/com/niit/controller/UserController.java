6d48183cadca33247a96b8fdcf286f03e3b9cd9d
Administrator <Administrator@MUM00127MR11.NIITANDHERI.COM>
2020-01-11 08:40:33 +0530

    changes

--- src/main/java/com/niit/controller/UserController.java
+++ src/main/java/com/niit/controller/UserController.java
@@ -2,6 +2,7 @@ package com.niit.controller;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -12,35 +13,37 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.niit.daoimpl.UserDAOImpl;
 import com.niit.model.User;
+import com.niit.service.UserService;
 
-@Controller
+@RestController
 @RequestMapping("/user")
 public class UserController 
 {	
-	UserDAOImpl udi=new UserDAOImpl();
+	@Autowired
+	UserService userService;
 	
 	@GetMapping("/display")
 	//@ResponseBody
 	public ResponseEntity<List<User>> getUsers()
 	{
-		return new ResponseEntity<List<User>>(udi.displayUsers(),HttpStatus.OK);
+		return new ResponseEntity<List<User>>(userService.displayUsers(),HttpStatus.OK);
 	}
-	
 	
 	@GetMapping("/display/{userid}")
 	//@ResponseBody
 	public ResponseEntity<User> getUserById(@PathVariable("userid") int userid)
 	{
-		return new ResponseEntity<User>(udi.displayUserById(userid),HttpStatus.OK);
+		return new ResponseEntity<User>(userService.displayUserById(userid),HttpStatus.OK);
 	}
 	
 	@PostMapping("/add")
 	public ResponseEntity<Void> addUser(@RequestBody User user)
 	{
-		udi.addUser(user);
+		userService.addUser(user);
 		return new ResponseEntity<Void>(HttpStatus.CREATED);
 	}
 	
@@ -48,14 +51,14 @@ public class UserController
 	@DeleteMapping("/delete/{userid}")
 	public ResponseEntity<Void> deleteUser(@PathVariable("userid") int userid )
 	{
-		udi.deleteUser(userid);
+		userService.deleteUser(userid);
 		return new ResponseEntity<Void>(HttpStatus.OK);
 	}
 	
 	@PutMapping("/update")
 	public ResponseEntity<Void> updateUser(@RequestBody User user)
 	{
-		udi.updateUser(user);
+		userService.updateUser(user);
 		return new ResponseEntity<Void>(HttpStatus.OK);
 	}
 	
